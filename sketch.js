d24b5984287befe0ee52bf0531688c7ef0bebd11
Linchenweiwei <150638876+Linchenweiwei@users.noreply.github.com>
2024-04-29 10:30:27 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,32 @@
+var captureGraphics
+var capture_width = 640
+var capture_height = 480
+var span = 5
+
+
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
   capture = createCapture(VIDEO)
-capture.size(320,240);//設定顯示畫面大小
+capture.size(capture_width,capture_height);//設定顯示畫面大小
+captureGraphics = createGraphics(capture_width,capture_height)
+captureGraphics.translate(capture_width,0)
+captureGraphics.scale(-1,1)
+
 }
 
 function draw() {
   background(220);
-  translate(width/2-160,height/2-120)
-  image(capture,0,0)
+  noStroke()
+  push
+  translate(width/2-capture_width/2,height/2-capture_height/2)
+  captureGraphics.image(capture,0,0)
+  for(var x = 0;x<captureGraphics.width;x=x+10){
+   for(var y=0;y<captureGraphics.height ; y=y+span){
+    var pixel = captureGraphics.get(x,y)
+    fill(pixel)
+    rect(x,y,span)
+    }
+}
 pop()
 }
